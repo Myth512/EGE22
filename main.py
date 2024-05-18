13fbd31d9c7c8abc166d33ce89a1722808684d84
Myth512 <122586261+Myth512@users.noreply.github.com>
2024-05-18 16:01:26 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,18 +3,24 @@ from itertools import *
 def calc():
     for i in tasks:
         tasks[i].end = tasks[i].time + max([tasks[e].end for e in tasks[i].deps]) + tasks[i].delay
-        if tasks[i].end > 38:
+        if tasks[i].end > max_len:
             return False
     return True
 
 
-def show_timeline():
-    timeline = [0] * 38
+def build_timeline():
+    timeline = [0] * max_len
     for i in tasks:
         for j in range(tasks[i].end - tasks[i].time, tasks[i].end):
             timeline[j] += 1
     return timeline
 
+
+def get_max_len(timeline, number_of_task):
+    for i in range(len(timeline)):
+        if timeline[i] != number_of_task:
+            timeline[i] = ' '
+    return max(map(len,''.join(map(str,timeline)).split()))
 
 class Task:
     def __init__(self, time, deps, ranges=range(1), delay=0, end=0):
@@ -41,10 +47,16 @@ tasks = {0: Task(0, [0]),
          13: Task(5, [10, 11], range(2))
 }
 
+number_of_tasks = 5
+
 tasks = dict(sorted(tasks.items(), key=lambda x: max(x[1].deps)))
 
 mx = 0
 current = 0
+
+max_len = 10000
+calc()
+max_len = max(i.end for i in tasks.values())
 
 range_values = [i.ranges for i in tasks.values()]
 all_combinations = product(*range_values)
@@ -53,8 +65,8 @@ for comb in all_combinations:
     delays = dict(zip(tasks, comb))
     for i in tasks:
         tasks[i].delay = delays[i]
-    if calc():
-        current = show_timeline().count(5)
+    if calc() and build_timeline().count(number_of_tasks):
+        current = get_max_len(build_timeline(), number_of_tasks)
         if current > mx:
             mx = current
-            print(mx, delays, show_timeline(), "\n")
+            print(mx, delays, build_timeline())
