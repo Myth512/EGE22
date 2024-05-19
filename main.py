edf69c0773a7766707da5bacb11aa3376d1f143c
Myth512 <122586261+Myth512@users.noreply.github.com>
2024-05-19 18:16:31 +0300

    Update main.py
    significant improve in speed

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from itertools import *
 
 def calc():
     for i in tasks:
-        tasks[i].end = tasks[i].time + max([tasks[e].end for e in tasks[i].deps]) + tasks[i].delay
+        tasks[i].end = tasks[i].time + max([tasks[e].end for e in tasks[i].deps], default=0) + tasks[i].delay
         if tasks[i].end > max_len:
             return False
     return True
@@ -20,53 +20,54 @@ def get_max_len(timeline, number_of_task):
     for i in range(len(timeline)):
         if timeline[i] != number_of_task:
             timeline[i] = ' '
-    return max(map(len,''.join(map(str,timeline)).split()))
+    return max(map(len,''.join(map(str,timeline)).split()), default=0)
 
 class Task:
-    def __init__(self, time, deps, ranges=range(1), delay=0, end=0):
+    def __init__(self, time, deps, delay=0, end=0):
         self.deps = deps
         self.time = time
-        self.ranges = ranges
         self.delay = 0
         self.end = 0
 
 
-tasks = {0: Task(0, [0]),
-         1: Task(5, [0], range(5)),
-         2: Task(4, [0], range(5)),
-         3: Task(8, [1, 2], range(2)),
-         4: Task(7, [3], range(2)),
-         5: Task(8, [3], range(2)),
-         6: Task(4, [4], range(2)),
-         7: Task(3, [5, 6], range(2)),
-         8: Task(1, [7], range(2)),
-         9: Task(9, [0], range(10, 15)),
-         10: Task(8, [0], range(10, 15)),
-         11: Task(7, [0], range(10, 15)),
-         12: Task(6, [10], range(2)),
-         13: Task(5, [10, 11], range(2))
+tasks = {1: Task(3, []),
+         2: Task(6, []),
+         3: Task(7, [1, 2]),
+         4: Task(10, [3]),
+         5: Task(6, [3]),
+         6: Task(8, [4, 5]),
+         7: Task(7, [4]),
+         8: Task(3, [6]),
+         9: Task(1, [7, 8]),
+         10: Task(4, []),
+         11: Task(4, []),
+         12: Task(2, [10]),
+         13: Task(2, [12]),
+         14: Task(11, [13]),
+         15: Task(4, [8, 11])
 }
 
-number_of_tasks = 5
+number_of_tasks = 3
 
-tasks = dict(sorted(tasks.items(), key=lambda x: max(x[1].deps)))
+tasks = dict(sorted(tasks.items(), key=lambda x: max(x[1].deps, default=0)))
 
 mx = 0
 current = 0
 
-max_len = 10000
-calc()
-max_len = max(i.end for i in tasks.values())
+max_len = 39
 
-range_values = [i.ranges for i in tasks.values()]
-all_combinations = product(*range_values)
+N = 3
 
-for comb in all_combinations:
-    delays = dict(zip(tasks, comb))
+for pr in permutations(tasks, r=N):
     for i in tasks:
-        tasks[i].delay = delays[i]
-    if calc() and build_timeline().count(number_of_tasks):
-        current = get_max_len(build_timeline(), number_of_tasks)
-        if current > mx:
-            mx = current
-            print(mx, delays, build_timeline())
+        tasks[i].delay = 0
+    all_combinations = product(range(max_len), repeat=N)
+    for comb in all_combinations:
+        delays = dict(zip(pr, comb))
+        for i in pr:
+            tasks[i].delay = delays[i]
+        if calc():
+            current = get_max_len(build_timeline(), number_of_tasks)
+            if current > mx:
+                mx = current
+                print(mx, delays)
