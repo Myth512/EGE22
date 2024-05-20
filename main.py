58390fa61ca6ec68d46fb65ec569a772e2e9b783
Myth512 <122586261+Myth512@users.noreply.github.com>
2024-05-20 17:16:54 +0300

    Update main.py
    increase readability

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ def calc():
     for task in tasks:
         max_dep = max([tasks[i-1].end for i in task.deps], default=0)
         task.end = task.duration + max_dep + task.delay
-        if task.end > max_len:
+        if task.end > threshold:
             return False
     return True
 
 
 def build_timeline():
-    timeline = [0] * max_len
+    timeline = [0] * threshold
     for task in tasks:
         for i in range(task.end - task.duration, task.end):
             timeline[i] += 1
@@ -45,25 +45,26 @@ tasks = [Task(2, []),
          Task(5, []),
          Task(9, [12])]
 
-number_of_tasks = 3
+target_number_of_tasks = 3
 
 # порядок в котором надо обрабатывать процессы
 order = [tasks.index(i) for i in sorted(tasks, key=lambda x: max(x.deps, default=0))]
 
-max_len = 24
+# сейчас это магическое число, но я потом пофикшу
+threshold = 24 # максимальная длина таймлайна
 
-mx = 0
-current = 0
+max_length = 0
+current_length = 0
 
 for N in range(5):
     for task_combinations in combinations(order, r=N):
         for task in tasks:
             task.delay = 0
-        for delay_combinations in product(range(1, max_len), repeat=N):
+        for delay_combinations in product(range(1, threshold), repeat=N):
             for task, delay in zip(task_combinations, delay_combinations):
                 tasks[task].delay = delay
             if calc():
-                current = get_max_len(build_timeline(), number_of_tasks)
-                if current > mx:
-                    mx = current
-                    print(mx, {r+1: tasks[r].delay for r in task_combinations})
+                current_length = get_max_len(build_timeline(), target_number_of_tasks)
+                if current_length > max_length:
+                    max_length = current_length
+                    print(max_length, {r+1: tasks[r].delay for r in task_combinations})
