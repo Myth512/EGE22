b845cab6b759c418e0ea76e1f442c98524802e45
Myth512 <122586261+Myth512@users.noreply.github.com>
2024-05-21 20:26:53 +0300

    Update main.py
    increase speed

--- main.py
+++ main.py
@@ -1,17 +1,16 @@
 from itertools import *
 
-
 def calc():
     '''функция производит вычисления положения процессов на таймлайне'''
-    for task in tasks:
-        max_dep = max([tasks[i-1].end for i in task.deps], default=0)
-        task.end = task.duration + max_dep + task.delay
-        if task.end > threshold:
+    for i in order:
+        max_dep = max([tasks[e-1].end for e in tasks[i].deps], default=0)
+        tasks[i].end = tasks[i].delay + max_dep + tasks[i].duration
+        if tasks[i].end > threshold:
             return False
     return True
 
 
-def build_timeline():
+def timeline():
     '''функция строит таймлайн в ввиде массива чисел,
      отражающих кол-во исполняемых процеcсов в момент времени'''
     timeline = [0] * threshold
@@ -21,9 +20,9 @@ def build_timeline():
     return timeline
 
 
-def get_max_len(timeline, number):
+def max_section(timeline, number):
     '''функция возвращает длину максимального отрезка, состоящего только из числа number'''
-    for i in range(len(timeline)):
+    for i in range(threshold):
         if timeline[i] != number:
             timeline[i] = ' '
     return max(map(len, ''.join(map(str, timeline)).split()), default=0)
@@ -37,27 +36,28 @@ class Task:
         self.end = None
 
 
-tasks = [Task(2, []),
-         Task(5, [1]),
-         Task(6, [1]),
-         Task(3, [2, 3]),
-         Task(8, [4]),
-         Task(5, [4]),
-         Task(2, [6]),
-         Task(3, [5, 7]),
-         Task(7, []),
-         Task(6, [9]),
-         Task(4, [9]),
-         Task(5, []),
-         Task(9, [12])]
+tasks = [Task(10, []),
+         Task(12, []),
+         Task(6, [1, 2]),
+         Task(12, [3]),
+         Task(16, [3]),
+         Task(10, [3]),
+         Task(9, []),
+         Task(9, []),
+         Task(7, [7, 8]),
+         Task(3, [9]),
+         Task(8, []),
+         Task(8, [11])]
 
-target_number_of_tasks = 3
+
+# количество процессов из условия
+target_number_of_tasks = 6
 
 # порядок в котором надо обрабатывать процессы
 order = [tasks.index(i) for i in sorted(tasks, key=lambda x: max(x.deps, default=0))]
 
 # сейчас это магическое число, но я потом пофикшу
-threshold = 24 # максимальная длина таймлайна
+threshold = 37 # максимальная длина таймлайна
 
 max_length = 0
 current_length = 0
@@ -67,10 +67,10 @@ for number_of_tasks in range(5):
         for task in tasks:
             task.delay = 0
         for delay_combinations in product(range(1, threshold), repeat=number_of_tasks):
-            for task, delay in zip(task_combinations, delay_combinations):
-                tasks[task].delay = delay
+            for i, delay in zip(task_combinations, delay_combinations):
+                tasks[i].delay = delay
             if calc():
-                current_length = get_max_len(build_timeline(), target_number_of_tasks)
+                current_length = max_section(timeline(), target_number_of_tasks)
                 if current_length > max_length:
                     max_length = current_length
-                    print(max_length, {i+1: tasks[i].delay for i in task_combinations})
+                    print(max_length, {i+1: tasks[i].delay for i in task_combinations}, timeline())
